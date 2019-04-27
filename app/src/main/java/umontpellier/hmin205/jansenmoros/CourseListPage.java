3b4a8d8653fa284a200dcdae0af5830d92ed97b5
BiancaJvRensburg <34648959+BiancaJvRensburg@users.noreply.github.com>
2019-04-27 22:01:12 +0200

    addCourses

--- app/src/main/java/umontpellier/hmin205/jansenmoros/CourseListPage.java
+++ app/src/main/java/umontpellier/hmin205/jansenmoros/CourseListPage.java
@@ -1,14 +1,19 @@
 package umontpellier.hmin205.jansenmoros;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.EventLogTags;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -33,7 +38,11 @@ public class CourseListPage extends AppCompatActivity {
         Retrofit retrofit = RESTClient.getInstance();
         myAPI = retrofit.create(INodeJS.class);
 
-        final LinearLayout layout = findViewById(R.id.course_page_layout);
+        final LinearLayout layout = (LinearLayout) findViewById(R.id.course_page_layout);
+
+        addCourse(layout, "Title: course 1", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.");
+        addCourse(layout, "Title: course 2", "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.");
+        addCourse(layout, "Title: course 3", "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.");
 
         compositeDisposable.add(myAPI.getCourses(Properties.getInstance().getGrade())
                         .subscribeOn(Schedulers.io())
@@ -71,5 +80,17 @@ public class CourseListPage extends AppCompatActivity {
     protected void onDestroy() {
         compositeDisposable.clear();
         super.onDestroy();
+    }
+
+    private void addCourse(LinearLayout layout, String courseName, String courseDescription){
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final View courseItemView = inflater.inflate(R.layout.nestedlayout_course_item, null);
+        layout.addView(courseItemView, layout.getChildCount());
+
+        TextView tvName = courseItemView.findViewById(R.id.tvCourseName);
+        TextView tvDescription = courseItemView.findViewById(R.id.tvCourseDescription);
+
+        tvName.setText(courseName);
+        tvDescription.setText(courseDescription);
     }
 }
